da762464e434bc1b37aca22e96776062b3d54bc0
iijung <987kmj4@naver.com>
2022-08-08 11:31:23 +0900

    fix:

--- ft_parse_token.c
+++ ft_parse_token.c
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 04:50:52 by minjungk          #+#    #+#             */
-/*   Updated: 2022/08/08 10:24:16 by minjungk         ###   ########.fr       */
+/*   Updated: 2022/08/08 11:21:16 by iijung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,10 @@ static int	parse_c(t_token *token, char c)
 
 static int	parse_s(t_token *token, char *s)
 {
-	if (s == 0 && token->opt.length < (int)ft_strlen("(null)"))
-		token->opt.length = 0;
+	if (s == 0 && token->opt.precision && token->opt.length < (int)ft_strlen("(null)"))
+		s = "";
 	else if (s == 0)
-	{
 		s = "(null)";
-		token->opt.length = 6;
-	}
 	if (token->opt.precision == 0 || token->opt.length >= (int)ft_strlen(s))
 		token->opt.length = ft_strlen(s);
 	if (token->opt.width < token->opt.length)
@@ -90,8 +87,11 @@ static int	parse_puxX(t_token *token, unsigned long ul)
 	char	*tmp;
 
 	if (token->opt.type == 'p' && ul == 0)
-		tmp = ft_strdup("(nil)");
-	else if (token->opt.type == 'u')
+	{
+		token->out = ft_strdup("(nil)");
+		return (0);
+	}
+	if (token->opt.type == 'u')
 		tmp = ft_utoa(ul, "0123456789");
 	else
 		tmp = ft_utoa(ul, "0123456789abcdef");
@@ -113,11 +113,38 @@ static int	parse_puxX(t_token *token, unsigned long ul)
 	else
 		ft_memcpy(token->out + token->opt.width - token->opt.length, \
 				tmp, token->opt.length);
-	if (token->opt.found && token->opt.minus)
+	if (token->opt.found && token->opt.minus && ul)
 		ft_memcpy(token->out, "0x", 2);
-	if (token->opt.found && token->opt.minus == 0)
+	if (token->opt.found && token->opt.minus == 0 && ul)
 		ft_memcpy(token->out + token->opt.width - token->opt.length - 2, \
 				"0x", 2);
+	free(tmp);
+	return (0);
+}
+
+static int	parse_di(t_token *token, int num)
+{
+	int	flag;
+	char	*tmp;
+
+	tmp = ft_itoa(num);
+	flag = num >= 0 && (token->opt.blank || token->opt.plus);
+	if (token->opt.width < (int)ft_strlen(tmp) + flag)
+		token->opt.width = ft_strlen(tmp) + flag;
+	token->out = ft_calloc(token->opt.width + 1, sizeof(char));
+	if (token->out == 0)
+	{
+		free(tmp);
+		return (-1);
+	}
+	if (token->opt.minus == 0 && token->opt.zero)
+		ft_memset(token->out, '0', token->opt.width);
+	else
+		ft_memset(token->out, ' ', token->opt.width);
+	if (token->opt.minus)
+		ft_memcpy(token->out + flag, tmp, token->opt.length);
+	else
+		ft_memcpy(token->out + token->opt.width - ft_strlen(tmp), tmp, ft_strlen(tmp));
 	free(tmp);
 	return (0);
 }
@@ -134,8 +161,8 @@ int	ft_parse_token(t_token *token, va_list ap)
 		return (parse_c(token, va_arg(ap, int)));
 	else if (token->opt.type == 's')
 		return (parse_s(token, va_arg(ap, char *)));
-//	else if (token->opt.type == 'd' || token->opt.type == 'i')
-//		token->out = ft_itoa(va_arg(ap, int));
+	else if (token->opt.type == 'd' || token->opt.type == 'i')
+		return (parse_di(token, va_arg(ap, int)));
 	else if (token->opt.type == 'u')
 		return (parse_puxX(token, va_arg(ap, unsigned int)));
 	else if (token->opt.type == 'x' || token->opt.type == 'X')
